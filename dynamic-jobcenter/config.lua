206d2dc3434eefe29cfe1525e595f68d21f21901
shynu S.J <shynujose11@gmail.com>
2023-05-16 00:10:31 +0530

    finzlized ui

---
+++ dynamic-jobcenter/config.lua
@@ -0,0 +1,63 @@
+Config = {}
+Config.Locations = {
+    {
+        position = vector3(-236.68, -919.76, 32.2),
+        jobs = {
+            {
+                name = "courier",
+                label = "Courier",
+                description = "Collect the Courier and Deliver to the NPC's",
+                icon = "mdi-truck-delivery"
+            },
+            {
+                name = "gardener",
+                label = "Gardener",
+                description = "Decorate the Garden and make money from it",
+                icon = "mdi-shovel"
+            },
+            {
+                name = "fisherman",
+                label = "Fisherman",
+                description = "Catch variety of Fishes and sell it to Merchants!",
+                icon = "mdi-fish"
+            },
+            {
+                name = "fruitpicker",
+                label = "Fruit Picker",
+                description = "Pluck variety of fruits, Wash it and sell to retailers",
+                icon = "mdi-fruit-pineapple"
+            },
+            {
+                name = "miner",
+                label = "Miner",
+                description = "Mine the rocks, extract into the minerals and sell them",
+                icon = "mdi-pickaxe"
+            },
+            {
+                name = "salvage",
+                label = "Salvage",
+                description = "Dive into the deep sea and collect Salvage wreck",
+                icon = "mdi-diving-helmet"
+            },
+            {
+                name = "tastyeats",
+                label = "Tasty Eats",
+                description = "Collect the Food and Deliver to the NPC's",
+                icon = "mdi-food"
+            },
+            {
+                name = "warehouse",
+                label = "WareHouse",
+                description = "Collect packages and deliver to targets",
+                icon = "mdi-warehouse"
+            },
+            {
+                name = "busdriver",
+                label = "Bus Driver",
+                description = "Drive Bus around the city and make money",
+                icon = "mdi-bus"
+            },
+        }
+    }
+}
+
---
+++ dynamic-jobcenter/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+
+author 'DYNAMIC#4928'
+description 'best ui for jobcenter..'
+version "1.0.0"
+
+shared_scripts {"config.lua"}
+client_scripts {"client/main.lua"}
+server_scripts {"server/main.lua"}
+
+files {"html/index.html", "html/style.css", "html/libs/vue.js"}
+
+ui_page "html/index.html"
